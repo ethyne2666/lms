bdb1292a03358ea0d69b082df03ebffd31988f48
temp <temporary2666@gmail.com>
2026-07-19 09:47:26 +0530

    added

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
-    <div>Educator</div>
+    <div>
+        Educator
+    <div>
+         {<Outlet />}
+    </div>
+    </div>
+    
   )
 }
 
